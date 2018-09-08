fdc508cfe3fc68ca31b61521df342174a2e1d010
Tichaona Hwandaza <tichaona@yahoo.com>
2018-09-08 14:34:02 -0600

    refactored

--- HwandazaHttpServer/ServerUtils/RestHandler.cs
+++ HwandazaHttpServer/ServerUtils/RestHandler.cs
@@ -42,7 +42,7 @@ namespace HwandazaHttpServer.ServerUtils
                 }
                 else
                 {
-                    responseContent = "{Error:" + appServiceResponse.Message + "}";
+                    responseContent = "{}";
                 }
             }
 
