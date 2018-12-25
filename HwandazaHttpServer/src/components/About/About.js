5bffb820ede39817d68520fc512c1ec4be2a69a1
Tichaona Hwandaza <tichaona@yahoo.com>
2018-12-24 18:22:46 -0600

    about template

--- HwandazaHttpServer/src/components/About/About.js
+++ HwandazaHttpServer/src/components/About/About.js
@@ -4,7 +4,76 @@ import "../../styles/css/styles.css";
 import './About.css';
 
 const about = props => (
-    <div className="hwandaza-automation">About goes here</div>
+<div className="row">
+    <div className="columns medium-centered col-sm-9">
+        <div className="container fluid">
+        <div className="hwandaza-automation">
+            <h2>About</h2>
+        </div>
+            <div className="columns small-centered small-10 medium-6 large-4">
+                <ol>
+                <li>
+                     <p>Better Business Zimbabwe is dedicated to promoting good business practices in
+                    Zimbabwe.
+
+                    Adding a review is simple, click on Add Review. If you are not logged in you
+                    will be automatically led to login screen!</p>
+                    </li>
+                    <ol>
+        <li>
+            <a href="https://facebook.github.io/react">React</a> - React JavaScript framework that I used.
+        </li>
+    </ol>
+    <ol>
+        <li>
+            <a href="https://github.com/tichaonax/HwandazaWebService">GitHub</a> - This codebase is stored in GitHub.
+        </li>
+    </ol>
+    <ol>
+                    <li>
+                        <h3><b>Status</b></h3>
+                        <p>Status comment.</p>
+                     </li>
+                </ol>
+                <ol>
+                    <li>
+                        <h3><b>Lights</b></h3>
+                        <p>A review consists of the </p>
+                    </li>
+                    </ol>
+                <ol>
+                    <li>
+                        <h3><b>Control</b></h3>
+                        <p>Control Information One</p>
+                        <p>Control Information Two</p>
+                    </li>
+                    </ol>
+                <ol>
+                    <li>
+                        <h3><b>Music</b></h3>
+                        <p>A review consists of the </p>
+                        
+                    </li>
+                    </ol>
+                <ol>
+                    <li>
+                        <h3><b>Gallery</b></h3>
+                        <p>Gallery Information One</p>
+                        <p>Gallery Information Two</p>
+                    </li>
+                    </ol>
+                <ol>
+                    <li>
+                        <h3><b>Settings</b></h3>
+                        <p>A review consists of the </p>
+                        
+                    </li>
+                    </ol>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
 );
 
 export default about;
