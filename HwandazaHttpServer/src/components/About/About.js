3d439dea2b4a5ec182205bdf0a1f5b116ccb57da
Tichaona Hwandaza <tichaona@yahoo.com>
2019-01-01 12:00:29 -0600

    added explanations to the About page

--- HwandazaHttpServer/src/components/About/About.js
+++ HwandazaHttpServer/src/components/About/About.js
@@ -10,7 +10,7 @@ browserNavigation('about');
 
 return(
 <div className="row">
-    <div className="columns medium-centered col-sm-9">
+    <div className="columns medium-centered col-sm-9 about">
         <div className="container fluid">
             <div className="hwandaza-automation">
                 <h2>About</h2>
@@ -18,9 +18,11 @@ return(
             <div className="columns small-centered small-10 medium-6 large-4">
                 <ol>
                     <li>
-                        <p>Hwandaza Automation runs on Raspbery Pi-III to automate some of the home operations such as pumping water.</p>
-                        <p>The Software, raspbery pi interface hardware circuits, design and implementation was done by Tichaona Hwandaza</p>
-                        <p>The Software comprises of Windows iOT foreground application and a background application that runs a Web Server</p>
+                        <p>Hwandaza Automation runs on Raspberry Pi 3 Model B to automate some of the home operations such as pumping water.</p>
+                        <p>The Software, Raspbery Pi Interface Hardware circuits, design and implementation was done by Tichaona Hwandaza</p>
+                        <p>The Software comprises of Windows 10 IoT foreground application and a background application that runs a Web Server.
+                            The webserber takes commands from a React client application then sends them to the fororeground application for processing.
+                        </p>
                     </li>
                     <ol>
                         <li>
@@ -29,49 +31,63 @@ return(
                     </ol>
                     <ol>
                         <li>
-                            <a href="https://github.com/tichaonax/HwandazaWebService">GitHub</a> - This codebase is stored in GitHub.
+                            <a href="https://github.com/tichaonax/HwandazaWebService">GitHub</a> - HwandazaWebService codebase is stored in GitHub.
                         </li>
                     </ol>
-    <ol>
+                    <ol>
+                        <li>
+                            <a href="https://github.com/tichaonax/HwandazaHttpServer">GitHub</a> - HwandazaHttpServer codebase is stored in GitHub.
+                        </li>
+                    </ol>
+                <ol>
                     <li>
                         <h3><b>Status</b></h3>
-                        <p>Status comment.</p>
+                        <p>This screen shows the status of controled items. The switches are read only.</p>
+                        <p>The top shows the date and time since the system was restarted.</p>
+                        <p>The left controlled items have a time stamp of last status change for the controlled device.</p>
                      </li>
                 </ol>
                 <ol>
                     <li>
                         <h3><b>Lights</b></h3>
-                        <p>A review consists of the </p>
+                        <p>This allows you to manually turn on the lights.</p>
+                        <p>This system randomly turns on/off these lights at night for predetermined period to simulate a 
+                            person moving in the house at night.</p>
                     </li>
                     </ol>
                 <ol>
                     <li>
                         <h3><b>Control</b></h3>
-                        <p>Control Information One</p>
-                        <p>Control Information Two</p>
+                        <p>The pumps can be turned on/off only on thisscreen. Each pump will run for a predetrmined time and shut off.
+                            The predetermined schedule is shown on the device screen.
+                        </p>
+                        <p>Water Pump: runs for 4 minutes</p>
+                        <p>FishPond: runs for 30 minutes</p>
+                        <p>Lawn Irrigator: runs for 4 minutes</p>
                     </li>
-                    </ol>
+                </ol>
                 <ol>
                     <li>
                         <h3><b>Music</b></h3>
-                        <p>A review consists of the </p>
-                        
+                        <p>The UI for playing music, the tracks are stored at the Raspbery Pi device.</p>
+                        <p>User can search music</p>
+                        <p>User can add playing music to favorites stored locally to the browser</p>
                     </li>
-                    </ol>
+                </ol>
                 <ol>
                     <li>
                         <h3><b>Gallery</b></h3>
-                        <p>Gallery Information One</p>
-                        <p>Gallery Information Two</p>
+                        <p>View images stored on the device, these are the same images that rotate on the device as background images</p>
                     </li>
                     </ol>
                 <ol>
                     <li>
                         <h3><b>Settings</b></h3>
-                        <p>A review consists of the </p>
-                        
+                        <p>Allows to change the system Date and Time</p>
+                        <p>Changing Date occurs immediately</p>
+                        <p>Changing Time requres the application to restart so the scehdules can reconfigure, changing Date does not reset the application</p>
                     </li>
-                    </ol>
+                </ol>
                 </ol>
             </div>
         </div>
