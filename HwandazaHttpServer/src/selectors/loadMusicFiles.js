e801cb725db87963893b287079ce633a9357c355
Tichaona Hwandaza <tichaona@yahoo.com>
2019-01-26 18:07:22 -0600

    fixed bug

--- HwandazaHttpServer/src/selectors/loadMusicFiles.js
+++ HwandazaHttpServer/src/selectors/loadMusicFiles.js
@@ -1,15 +1,13 @@
 export function loadMusicFiles(songList) {
     console.log('loadMusicFiles', songList);
+    debugger;
     const songs = songList.result.map(song => {
         //localstorage data may be corrupt wrap inside a try catch
         try {
-            //{ ".m4a", ".mp3", ".wma" }
             const parts = song.Url.split('/');
             const artist = unescape(parts[0]);
             let namepart = unescape(parts[parts.length-1]);
-            let mp4Str = namepart.substring(0, namepart.indexOf(".mp4"));
-            let mp3Str = mp4Str.substring(0, mp4Str.indexOf(".mp3"));
-            let name = mp3Str.substring(0, mp3Str.indexOf(".wma"));
+            let name = namepart.substring(0, namepart.indexOf("."));
             return ({
                 cover: `picture/${song.Cover}`,
                 url: `song/${song.Url}`,
